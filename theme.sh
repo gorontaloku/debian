9d25779f165220693219b352c60b6a39f91ff237
gorontaloku <106912043+gorontaloku@users.noreply.github.com>
2024-09-03 09:25:14 +0700

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -37,9 +37,6 @@ rm Meslo.zip
 rm LICENSE.txt
 rm readme.md
 
-wget https://github.com/wahyu22010/Debian/raw/main/NotoColorEmoji-Regular.ttf
-mv NotoColorEmoji-Regular.ttf .fonts
-
 wget https://github.com/wahyu22010/Debian/raw/main/font.ttf
 mv font.ttf .termux/font.ttf
 
