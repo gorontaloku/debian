05e25182f8ab264bc664c3336d4b8c4aab26d070
gorontaloku <106912043+gorontaloku@users.noreply.github.com>
2024-12-09 16:30:40 +0800

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -97,7 +97,7 @@ source $PREFIX/etc/bash.bashrc
 termux-reload-settings
 
 #Downloads File wpsoffice
-Wget https://github.com/Pi-Apps-Coders/files/releases/download/large-files/wps-office_11.1.0.11720_arm64.deb
+wget https://github.com/Pi-Apps-Coders/files/releases/download/large-files/wps-office_11.1.0.11720_arm64.deb
 mv wps-office_11.1.0.11720_arm64.deb $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username/
 
 #Download File Github Desktop
