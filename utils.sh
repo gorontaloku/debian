832cc3a86698dbbab1bee6ca115d657af2113f29
gorontaloku <106912043+gorontaloku@users.noreply.github.com>
2025-04-26 18:34:17 +0800

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -124,54 +124,27 @@ chmod +x start
 mv start $PREFIX/bin
 
 #Shutdown Utility
-cat <<'EOF' > $PREFIX/bin/kill_termux_x11
+cat <<'EOF' > stop
 #!/bin/bash
 
-# Check if Apt, dpkg, or Nala is running in Termux or Proot
-if pgrep -f 'apt|apt-get|dpkg|nala'; then
-  zenity --info --text="Software is currently installing in Termux or Proot. Please wait for these processes to finish before continuing."
-  exit 1
-fi
-
-# Get the process IDs of Termux-X11 and XFCE sessions
-termux_x11_pid=$(pgrep -f /system/bin/app_process.*com.termux.x11.Loader)
-xfce_pid=$(pgrep -f "xfce4-session")
-
-# Add debug output
-echo "Termux-X11 PID: $termux_x11_pid"
-echo "XFCE PID: $xfce_pid"
-
-# Check if the process IDs exist
-if [ -n "$termux_x11_pid" ] && [ -n "$xfce_pid" ]; then
-  # Kill the processes
-  kill -9 "$termux_x11_pid" "$xfce_pid"
-  zenity --info --text="Termux-X11 and XFCE sessions closed."
-else
-  zenity --info --text="Termux-X11 or XFCE session not found."
-fi
-
-info_output=$(termux-info)
-pid=$(echo "$info_output" | grep -o 'TERMUX_APP_PID=[0-9]\+' | awk -F= '{print $2}')
-kill "$pid"
-
-exit 0
-
+pkill -f com.termux.x11
 
 EOF
 
-chmod +x $PREFIX/bin/kill_termux_x11
+chmod +x stop
+mv stop $PREFIX/bin
 
 #Create kill_termux_x11.desktop
 echo "[Desktop Entry]
 Version=1.0
 Type=Application
-Name=Kill Termux X11
+Name=stop
 Comment=
-Exec=kill_termux_x11
+Exec=pkill -f com.termux.x11
 Icon=system-shutdown
 Categories=System;
 Path=
 StartupNotify=false
-" > $HOME/Desktop/kill_termux_x11.desktop
-chmod +x $HOME/Desktop/kill_termux_x11.desktop
-mv $HOME/Desktop/kill_termux_x11.desktop $PREFIX/share/applications
+" > $HOME/Desktop/stop.desktop
+chmod +x $HOME/Desktop/stop.desktop
+mv $HOME/Desktop/stop.desktop $PREFIX/share/applications
