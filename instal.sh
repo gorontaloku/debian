4bbbf65639ecbc5c8a57faf23089f0c868346245
gorontaloku <106912043+gorontaloku@users.noreply.github.com>
2025-04-28 14:27:38 +0800

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -62,7 +62,7 @@ done
 
 clear
 
-pkgs=( 'wget' 'ncurses-utils' 'dbus' 'proot-distro' 'x11-repo' 'tur-repo' 'android-tools' 'pulseaudio')
+pkgs=( 'ncurses-utils' 'dbus' 'proot-distro' 'x11-repo' 'tur-repo' 'android-tools' 'pulseaudio')
 pkg uninstall dbus -y
 #pkg update
 pkg install "${pkgs[@]}" -y -o Dpkg::Options::="--force-confold"
